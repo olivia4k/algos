4e0b8c0887c3eecc0086a2c9887fe466856c180c
olivia4k <olivia.klepacka@gmail.com>
2021-10-18 19:13:48 +0200

    fix Two Sum

--- Two_Sum.py
+++ Two_Sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        set_ = {}  
+        dict_ = {}  
         
         for i, v in enumerate(nums):
             trace = target - v 
             
-            if trace in set_ :
-                return set_[trace], i
+            if trace in dict_ :
+                return dict_[trace], i
             
-            set_[v] = i
+            dict_[v] = i
